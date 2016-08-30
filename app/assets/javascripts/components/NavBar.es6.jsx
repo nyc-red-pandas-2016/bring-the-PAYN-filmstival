05838273eda6bffd3308cb19ecbb9225d87d7c7d
Chainsaw Bucktooth <email": null>
2016-08-30 11:24:14 -0400

    add logic to NavBar component to display different navbar menu depending on whether a current user is logged in or not

--- app/assets/javascripts/components/NavBar.es6.jsx
+++ app/assets/javascripts/components/NavBar.es6.jsx
@@ -4,13 +4,21 @@ class NavBar extends React.Component{
   render(){
     return(
       <nav className="navbar">
+        { this.props.currentUser ?
         <ul>
-          <li><a href='/users/sign_in'>Sign In</a></li>
-          <li><a href='/users/new'>Register</a></li>
           <li><a href='/users/sign_out'>Log Out</a></li>
-          <li><a href='rootpath'>Home</a></li>
+          <li><{this.props.currentUser.username}</li>
+          <li><a href='/'>Home</a></li>
           <SearchBoxView/>
         </ul>
+        :
+        <ul>
+          <li><a href='/users/sign_in'>Sign In</a></li>
+          <li><a href='/users/sign_up'>Register</a></li>
+          <li><a href='/'>Home</a></li>
+          <SearchBoxView/>
+        </ul>
+      }
       </nav>
     )
   }
