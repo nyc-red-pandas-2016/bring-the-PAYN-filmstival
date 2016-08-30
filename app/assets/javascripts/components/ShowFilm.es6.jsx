a0e48565eb5817c0b5efc432bf8c6d0a31886a8a
Chainsaw Bucktooth <email": null>
2016-08-30 16:23:43 -0400

    fixed things

---
+++ app/assets/javascripts/components/ShowFilm.es6.jsx
@@ -0,0 +1,25 @@
+class ShowFilm extends React.Component{
+    constructor(){
+      super();
+      this.state = {
+
+      }
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+      return(
+        <div className="appMain">
+          <NavBar currentUser = {this.props.currentUser}/>
+          <MovieItem movieItem = {this.props.movie}/>
+          <ReviewsList/>
+          review form - to create new comments on movie
+          <CommentsList/>
+          comment form - to create comments on review
+        </div>
+      )
+    }
+}
