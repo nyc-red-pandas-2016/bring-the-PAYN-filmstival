abafcedbc4393adcb48f0c6a0e0c7db53751f294
Chainsaw Bucktooth <email": null>
2016-08-31 14:35:59 -0400

    deconflict

--- app/assets/javascripts/components/MovieItem.es6.jsx
+++ app/assets/javascripts/components/MovieItem.es6.jsx
@@ -21,8 +21,6 @@ class MovieItem extends React.Component{
     let data = this.props.data
 
     return(
-      <div className="theMovie">
-
           <h4>{data.film.title}</h4>
           <p>{data.film.description}</p>
 
@@ -38,8 +36,6 @@ class MovieItem extends React.Component{
             <p id="voteTally">{this.props.data.votes.length} votes</p>
           </div>
         }
-
-      </div>
     )
   }
 }
